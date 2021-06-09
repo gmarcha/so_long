ac10a057dd5993b9464ceecf3c8dd64276ae03d6
gmarcha <gmarcha@github.com>
2021-06-09 16:27:06 +0200

    update so_long: add map init (open/read map)

--- src/so_long.c
+++ src/so_long.c
@@ -30,57 +30,148 @@ void	die(char *errmsg, int errnum)
 	exit(0);
 }
 
+void	game_destroy(t_game *game)
+{
+	int				i;
+
+	if (game != 0)
+	{
+		if (game->map != 0)
+		{
+			i = 0;
+			while (i < game->height)
+				free(game->map[i++]);
+			free(game->map);
+		}
+		if (game->obj_pos != 0)
+			free(game->obj_pos);
+		free(game);
+	}
+}
+
 void	root_destroy(t_root *root, char *errmsg, int errnum)
 {
-	if (root->mlx_img)	
-        mlx_destroy_image(root->mlx, root->mlx_img);
-	if (root->mlx_win)
-        mlx_destroy_window(root->mlx, root->mlx_win);
-	if (root->mlx)
-        mlx_destroy_display(root->mlx);
-	free(root);
+	if (root != 0)
+	{
+		if (root->mlx_img != 0)	
+			mlx_destroy_image(root->mlx, root->mlx_img);
+		if (root->mlx_win != 0)
+			mlx_destroy_window(root->mlx, root->mlx_win);
+		if (root->mlx != 0)
+			mlx_destroy_display(root->mlx);
+		if (root->game != 0)
+			game_destroy(root->game);
+		free(root);
+	}
 	die(errmsg, errnum);
 }
 
-t_root	*root_init(void)
+void	map_parsing(t_root *root, char *file)
+{
+	(void)root;
+	ft_putendl_fd(file, 1);	
+}
+
+void	map_init(t_root *root, char *filename)
+{
+	int				fd;
+	char			*file;
+	char			buf[1024 + 1];
+	int				ret;
+	char			*tmp;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		root_destroy(root, filename, errno);
+	file = ft_calloc(1, 1);
+	if (file == 0)
+	{
+		close(fd);
+		root_destroy(root, "map_init(): ft_calloc()", errno);
+	}
+	ret = 1;
+	while (ret != 0)
+	{
+		ret = read(fd, buf, 1024);
+		if (ret == -1)
+		{
+			free(file);
+			close(fd);
+			root_destroy(root, "map_init(): read()", errno);
+		}
+		else
+		{
+			buf[ret] = 0;
+			tmp = ft_strjoin(file, buf);
+			free(file);
+			file = tmp;
+			if (file == 0)
+			{
+				close(fd);
+				root_destroy(root, "map_init(): ft_strjoin()", errno);
+			}
+		}
+	}
+	close(fd);
+	map_parsing(root, file);
+	free(file);
+}
+
+void	game_init(t_root *root, char *filename)
+{
+	root->game = (t_game *)malloc(sizeof(t_game));
+	if (root->game == 0)
+		root_destroy(root, "game_init(): malloc()", errno);
+	root->game->map = 0;
+	root->game->obj_pos = 0;
+	map_init(root, filename);
+}
+
+t_root	*root_init(char *filename)
 {
 	t_root			*root;
 
 	root = (t_root *)malloc(sizeof(t_root));
 	if (root == 0)
-		die("root_init(): malloc()", errno);
+		root_destroy(0, "root_init(): malloc()", errno);
+	root->game = 0;
 	root->mlx = 0;
 	root->mlx_win = 0;
 	root->mlx_img = 0;
-    root->mlx = mlx_init();
-    if (root->mlx == 0)
-    	root_destroy(root, "mlx_init(): can't load mlx", 0);
-    root->mlx_win = mlx_new_window(root->mlx, 1920, 1080, "Hello world!");
-    if (root->mlx_win == 0)
+	game_init(root, filename);
+	root->mlx = mlx_init();
+	if (root->mlx == 0)
+		root_destroy(root, "mlx_init(): can't load mlx", 0);
+	root->mlx_win = mlx_new_window(root->mlx, 1920, 1080, "Hello world!");
+	if (root->mlx_win == 0)
 		root_destroy(root, "mlx_new_window(): can't create a window", 0);
-    root->mlx_img = mlx_new_image(root->mlx, 1920, 1080);
+	root->mlx_img = mlx_new_image(root->mlx, 1920, 1080);
 	if (root->mlx_img == 0)
 		root_destroy(root, "mlx_new_image(): can't create an image", 0);
 	return (root);
 }
 
+
+
 int    key_hook(int keycode, t_root *root)
 {
-    if (keycode == 65307)
-    {
+	if (keycode == 65307)
+	{
 		ft_putstr_fd("Thanks for playing!\nhttps://github.com/gmarcha\n", 1);
 		root_destroy(root, 0, 0);
-    }
-    return (0);
+	}
+	return (0);
 }
 
-int    main(void)
+int    main(int ac, char *av[])
 {
-    t_root          *root;
+	t_root          *root;
 
-    root = root_init();
-    mlx_put_image_to_window(root->mlx, root->mlx_win, root->mlx_img, 0, 0);
-    mlx_hook(root->mlx_win, 2, (1L << 0), &key_hook, root);
-    mlx_loop(root->mlx);
-    return (0);
+	if (ac != 2)
+		die("invalid number arguments", 0);
+	root = root_init(av[1]);
+	mlx_put_image_to_window(root->mlx, root->mlx_win, root->mlx_img, 0, 0);
+	mlx_hook(root->mlx_win, 2, (1L << 0), &key_hook, root);
+	mlx_loop(root->mlx);
+	return (0);
 }
