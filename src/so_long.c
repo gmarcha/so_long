2fa20f264a908f9de7ab2ec982831b9efed18f39
gmarcha <gmarcha@github.com>
2021-06-09 22:28:23 +0200

    update so_long: update map parsing

--- src/so_long.c
+++ src/so_long.c
@@ -3,14 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   so_long.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gamarcha <gamarcha@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lrandria <lrandria@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/07 18:36:29 by gamarcha          #+#    #+#             */
-/*   Updated: 2021/06/09 18:50:29 by gamarcha         ###   ########.fr       */
+/*   Updated: 2021/06/09 22:27:19 by lrandria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+void	free_matrix(int **m, int size)
+{
+	int				i;
+
+	i = 0;
+	while (i < size)
+		free(m[i++]);
+	free(m);
+}
 
 void	die(char *errmsg, int errnum)
 {
@@ -36,6 +46,8 @@ void	game_destroy(t_game *game)
 
 	if (game != 0)
 	{
+		if (game->obj_pos != 0)
+			free(game->obj_pos);
 		if (game->map != 0)
 		{
 			i = 0;
@@ -43,8 +55,6 @@ void	game_destroy(t_game *game)
 				free(game->map[i++]);
 			free(game->map);
 		}
-		if (game->obj_pos != 0)
-			free(game->obj_pos);
 		free(game);
 	}
 }
@@ -79,27 +89,133 @@ int		count_char(char *s, int c)
 	return (count);
 }
 
-void	map_parsing(t_root *root, char *file)
+void	map_width(t_root *root, char *file)
 {
-	int				count_line;
-	int				size_line;
+	root->game->width = 0;
+	while (file[root->game->width] && file[root->game->width] != '\n')
+		root->game->width++;
+	if (root->game->width == 0 || file[root->game->width] == 0)
+		root_destroy(root, "map file is invalid", 0);
+}
+
+void	map_height(t_root *root, char *file)
+{
 	int				i;
 	int				j;
 	
-	size_line = 0;
-	while (file[size_line] && file[size_line] != '\n')
-		size_line++;
-	if (size_line == 0)
-		root_destroy(root, "map is empty", )
-	count_line = 1;
-	i = size_line;
-	while (file[i])
+	root->game->height = 1;
+	i = root->game->width + 1;
+	while (file[i] != 0)
 	{
 		j = 0;
-		while (file[i] && file[i] != '\n')
+		while (file[i + j] != 0 && file[i + j] != '\n')
 			j++;
-		
+		if (root->game->width != j)
+			root_destroy(root, "map format is invalid", 0);
+		i += root->game->width + 1;
+		root->game->height++;
 	}
+}
+
+void	map_isvalid(t_root *root, char *file)
+{
+	int				count_player;
+	int				count_exit;
+	int				i;
+
+	count_player = 0;
+	count_exit = 0;
+	i = -1;
+	while (file[++i] != 0)
+	{
+		if (file[i] == '\n')
+			continue ;
+		if (i < root->game->width ||
+			i > (root->game->width + 1) * (root->game->height - 1) ||
+			i % (root->game->width + 1) == 0 ||
+			i % (root->game->width + 1) == root->game->width - 1)
+		{
+			if (file[i] != '1')
+				root_destroy(root, "map isn't surrounded by walls", 0);
+		}
+		else
+		{
+			if (file[i] == 'P')
+				count_player++;
+			else if (file[i] == 'E')
+				count_exit++;
+			else if (file[i] == 'C')
+				root->game->obj_count++;
+			else if (file[i] == '1' || file[i] == '0')	
+				continue ;
+			else
+				root_destroy(root, "map content is invalid", 0);
+		}
+	}
+	if (count_player != 1 || count_exit != 1 || root->game->obj_count < 1)
+		root_destroy(root, "map configuration is invalid", 0);
+}
+
+void	map_parsing(t_root *root, char *file)
+{
+	int				i;
+	int				j;
+	int				k;
+	int				obj;
+
+	root->game->obj_pos = (t_coord *)malloc(sizeof(t_coord) * root->game->obj_count);
+	if (root->game->obj_pos == 0)
+		root_destroy(root, "map_parsing(): malloc()", errno);
+	root->game->map = (int **)malloc(sizeof(int *) * root->game->height);
+	if (root->game->map == 0)
+		root_destroy(root, "map_parsing(): malloc()", errno);
+	obj = 0;
+	k = 0;
+	j = 0;
+	while (j < root->game->height)
+	{
+		root->game->map[j] = (int *)malloc(sizeof(int) * root->game->width);
+		if (root->game->map[j] == 0)
+		{
+			free_matrix(root->game->map, j);
+			root->game->map = 0;
+			root_destroy(root, "map_parsing(): malloc()", errno);
+		}
+		i = 0;
+		while (i < root->game->width)
+		{
+			if (file[k] == 'P')
+			{
+				root->game->player_pos.x = k % (root->game->width + 1);
+				root->game->player_pos.y = k / (root->game->width + 1);
+				file[k] = '0';
+			}
+			if (file[k] == 'E')
+			{
+				root->game->exit_pos.x = k % (root->game->width + 1);
+				root->game->exit_pos.y = k / (root->game->width + 1);
+				file[k] = '0';
+			}
+			if (file[k] == 'C')
+			{
+				root->game->obj_pos[obj].x = k % (root->game->width + 1);
+				root->game->obj_pos[obj].y = k / (root->game->width + 1);
+				obj++;
+				file[k] = '0';
+			}
+			root->game->map[j][i++] = file[k++] - 48;
+		}
+		k++;
+		j++;
+	}
+}
+
+void	map_read(t_root *root, char *file)
+{
+	map_width(root, file);
+	map_height(root, file);
+	map_isvalid(root, file);
+	map_parsing(root, file);
 }
 
 void	map_init(t_root *root, char *filename)
@@ -143,7 +259,7 @@ void	map_init(t_root *root, char *filename)
 		}
 	}
 	close(fd);
-	map_parsing(root, file);
+	map_read(root, file);
 	free(file);
 }
 
@@ -154,6 +270,7 @@ void	game_init(t_root *root, char *filename)
 		root_destroy(root, "game_init(): malloc()", errno);
 	root->game->map = 0;
 	root->game->obj_pos = 0;
+	root->game->obj_count = 0;
 	map_init(root, filename);
 }
 
