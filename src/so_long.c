e840c5c9e0cb7bdf772e6cfb057b077effe5166a
gmarcha <gmarcha@github.com>
2021-06-12 17:46:58 +0200

    update so_long: correction

--- src/so_long.c
+++ src/so_long.c
@@ -6,7 +6,7 @@
 /*   By: gamarcha <gamarcha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/07 18:36:29 by gamarcha          #+#    #+#             */
-/*   Updated: 2021/06/12 17:46:10 by gamarcha         ###   ########.fr       */
+/*   Updated: 2021/06/12 17:46:55 by gamarcha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -363,6 +363,7 @@ int    key_hook(int keycode, t_root *root)
 
 int	destroy_hook(int keycode, t_root *root)
 {
+	(void)keycode;
 	ft_putstr_fd("Thanks for playing!\nhttps://github.com/gmarcha\n", 1);
 	root_destroy(root, 0, 0);
 	return (0);
