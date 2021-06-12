4177f8680e9f6afd4b8ab0b5c4a68be8c7424d54
gmarcha <gmarcha@github.com>
2021-06-13 00:35:02 +0200

    update so long: add texture loading

--- src/so_long.c
+++ src/so_long.c
@@ -6,7 +6,7 @@
 /*   By: gamarcha <gamarcha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/07 18:36:29 by gamarcha          #+#    #+#             */
-/*   Updated: 2021/06/12 23:37:56 by gamarcha         ###   ########.fr       */
+/*   Updated: 2021/06/13 00:34:25 by gamarcha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,6 +63,16 @@ void	root_destroy(t_root *root, char *errmsg, int errnum)
 {
 	if (root != 0)
 	{
+		if (root->ground != 0)
+			mlx_destroy_image(root->mlx, root->ground);
+		if (root->wall != 0)
+			mlx_destroy_image(root->mlx, root->wall);
+		if (root->coll != 0)
+			mlx_destroy_image(root->mlx, root->coll);
+		if (root->exit != 0)
+			mlx_destroy_image(root->mlx, root->exit);
+		if (root->player != 0)
+			mlx_destroy_image(root->mlx, root->player);
 		if (root->mlx_img != 0)	
 			mlx_destroy_image(root->mlx, root->mlx_img);
 		if (root->mlx_win != 0)
@@ -321,6 +331,27 @@ void	game_init(t_root *root, char *filename)
 	map_init(root, filename);
 }
 
+void	texture_load(t_root *root, t_img **img, char *path)
+{
+	int				width;
+	int				height;
+
+	*img = mlx_xpm_file_to_image(root->mlx, path, &width, &height);
+	if (*img == 0)
+		root_destroy(0, "texture_init(): can't load texture", 0);
+	(*img)->width = width;
+	(*img)->height = height;
+}
+
+void	texture_init(t_root *root)
+{
+	texture_load(root, &root->player, "./img/player.xpm");
+	texture_load(root, &root->exit, "./img/exit.xpm");
+	texture_load(root, &root->coll, "./img/coll.xpm");
+	texture_load(root, &root->wall, "./img/wall.xpm");
+	texture_load(root, &root->ground, "./img/ground.xpm");
+}
+
 t_root	*root_init(char *filename)
 {
 	t_root			*root;
@@ -332,6 +363,11 @@ t_root	*root_init(char *filename)
 	root->mlx = 0;
 	root->mlx_win = 0;
 	root->mlx_img = 0;
+	root->player = 0;
+	root->exit = 0;
+	root->coll = 0;
+	root->wall = 0;
+	root->ground = 0;
 	game_init(root, filename);
 	root->mlx = mlx_init();
 	if (root->mlx == 0)
@@ -342,6 +378,7 @@ t_root	*root_init(char *filename)
 	root->mlx_img = mlx_new_image(root->mlx, root->game->width * 40, root->game->height * 40);
 	if (root->mlx_img == 0)
 		root_destroy(root, "mlx_new_image(): can't create an image", 0);
+	texture_init(root);
 	return (root);
 }
 
