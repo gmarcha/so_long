7f12b67c1a872a7c152c2cadbbccda237af7dfbd
gmarcha <gmarcha@github.com>
2021-06-13 04:21:06 +0200

    update so long: add main function

--- src/so_long.c
+++ src/so_long.c
@@ -6,29 +6,11 @@
 /*   By: gamarcha <gamarcha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/07 18:36:29 by gamarcha          #+#    #+#             */
-/*   Updated: 2021/06/13 04:13:23 by gamarcha         ###   ########.fr       */
+/*   Updated: 2021/06/13 04:20:01 by gamarcha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
-
-void	die(char *errmsg, int errnum)
-{
-	if (errmsg != 0 || errnum != 0)
-		ft_putendl_fd("Error", 2);
-	if (errmsg != 0)
-		ft_putstr_fd(errmsg, 2);
-	if (errmsg != 0 && errnum != 0)
-		ft_putstr_fd(": ", 2);
-	if (errnum != 0)
-		ft_putstr_fd(strerror(errnum), 2);
-	if (errmsg != 0 || errnum != 0)
-	{
-		ft_putendl_fd("", 2);
-		exit(1);
-	}
-	exit(0);
-}
 
 int	isber(char *file)
 {
