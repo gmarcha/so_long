ee1b03892399922d530a835134466882dd23e87a
gmarcha <gmarcha@github.com>
2021-06-13 04:57:25 +0200

    update mlx: mute warning

--- mlx/mlx_new_image.c
+++ mlx/mlx_new_image.c
@@ -23,8 +23,9 @@ int	mlx_X_error;
 
 int	shm_att_pb(Display *d,XErrorEvent *ev)
 {
+  int ret;
   if (ev->request_code==146 && ev->minor_code==X_ShmAttach)
-    write(2,WARN_SHM_ATTACH,strlen(WARN_SHM_ATTACH));
+    ret = write(2,WARN_SHM_ATTACH,strlen(WARN_SHM_ATTACH));
   mlx_X_error = 1;
 }
 
