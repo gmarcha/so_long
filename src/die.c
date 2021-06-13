6892847409d7fb119850c4ccf5f306c91a3d431a
gmarcha <gmarcha@github.com>
2021-06-13 04:20:39 +0200

    update so long: add exiting function

---
+++ src/die.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   die.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gamarcha <gamarcha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/13 04:20:06 by gamarcha          #+#    #+#             */
+/*   Updated: 2021/06/13 04:20:06 by gamarcha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+void	die(char *errmsg, int errnum)
+{
+	if (errmsg != 0 || errnum != 0)
+		ft_putendl_fd("Error", 2);
+	if (errmsg != 0)
+		ft_putstr_fd(errmsg, 2);
+	if (errmsg != 0 && errnum != 0)
+		ft_putstr_fd(": ", 2);
+	if (errnum != 0)
+		ft_putstr_fd(strerror(errnum), 2);
+	if (errmsg != 0 || errnum != 0)
+	{
+		ft_putendl_fd("", 2);
+		exit(1);
+	}
+	exit(0);
+}
