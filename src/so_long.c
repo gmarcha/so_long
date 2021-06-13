8da695bbae4b0bc3c7f77daa527c60109e200148
gmarcha <gmarcha@github.com>
2021-06-13 03:16:32 +0200

    update so long: add check .ber

--- src/so_long.c
+++ src/so_long.c
@@ -6,7 +6,7 @@
 /*   By: gamarcha <gamarcha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/07 18:36:29 by gamarcha          #+#    #+#             */
-/*   Updated: 2021/06/13 02:46:50 by gamarcha         ###   ########.fr       */
+/*   Updated: 2021/06/13 03:16:12 by gamarcha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -540,12 +540,28 @@ int	destroy_hook(int keycode, t_root *root)
 	return (0);
 }
 
-int    main(int ac, char *av[])
+int	isber(char *file)
 {
-	t_root          *root;
+	int				len;
+
+	len = ft_strlen(file);
+	if (file == 0)
+		return (0);
+	if (len < 5)
+		return (0);
+	if (ft_strcmp(file + len - 4, ".ber") != 0))
+		return (0);
+	return (1);
+}
+
+int	main(int ac, char *av[])
+{
+	t_root			*root;
 
 	if (ac != 2)
 		die("invalid number of arguments", 0);
+	if (isber(av[1]) == 0)
+		die("invalid argument (<name>.ber)", 0);
 	root = root_init(av[1]);
 	draw(root);
 	mlx_hook(root->mlx_win, 2, 1L << 0, key_press, root);
