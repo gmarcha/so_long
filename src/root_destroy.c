71e42bd9b457e2da386dbfc7dcee0858a6bf397c
gmarcha <gmarcha@github.com>
2021-06-13 04:17:10 +0200

    update so long: add root handling

---
+++ src/root_destroy.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   root_destroy.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gamarcha <gamarcha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/13 04:11:06 by gamarcha          #+#    #+#             */
+/*   Updated: 2021/06/13 04:11:06 by gamarcha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+void	root_destroy(t_root *root, char *errmsg, int errnum)
+{
+	if (root != 0)
+	{
+		if (root->ground != 0)
+			mlx_destroy_image(root->mlx, root->ground);
+		if (root->wall != 0)
+			mlx_destroy_image(root->mlx, root->wall);
+		if (root->coll != 0)
+			mlx_destroy_image(root->mlx, root->coll);
+		if (root->exit != 0)
+			mlx_destroy_image(root->mlx, root->exit);
+		if (root->player != 0)
+			mlx_destroy_image(root->mlx, root->player);
+		if (root->mlx_img != 0)	
+			mlx_destroy_image(root->mlx, root->mlx_img);
+		if (root->mlx_win != 0)
+			mlx_destroy_window(root->mlx, root->mlx_win);
+		if (root->mlx != 0)
+			mlx_destroy_display(root->mlx);
+		if (root->game != 0)
+			game_destroy(root->game);
+		free(root);
+	}
+	die(errmsg, errnum);
+}
---
+++ src/root_init.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   root_init.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gamarcha <gamarcha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/13 03:51:29 by gamarcha          #+#    #+#             */
+/*   Updated: 2021/06/13 03:51:29 by gamarcha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+static void	texture_load(t_root *root, t_img **img, char *path)
+{
+	int				width;
+	int				height;
+
+	*img = mlx_xpm_file_to_image(root->mlx, path, &width, &height);
+	if (*img == 0)
+		root_destroy(0, "texture_init(): can't load texture", 0);
+	(*img)->width = width;
+	(*img)->height = height;
+}
+
+static void	texture_init(t_root *root)
+{
+	texture_load(root, &root->player, "./img/player.xpm");
+	texture_load(root, &root->exit, "./img/moto.xpm");
+	texture_load(root, &root->coll, "./img/gas.xpm");
+	texture_load(root, &root->wall, "./img/wall.xpm");
+	texture_load(root, &root->ground, "./img/sand.xpm");
+}
+
+static void	renderer_init(t_root *root)
+{
+	root->mlx = mlx_init();
+	if (root->mlx == 0)
+		root_destroy(root, "mlx_init(): can't load mlx", 0);
+	root->mlx_win = mlx_new_window(root->mlx, root->game->width * 40,
+		root->game->height * 40, "so_long");
+	if (root->mlx_win == 0)
+		root_destroy(root, "mlx_new_window(): can't create a window", 0);
+	root->mlx_img = mlx_new_image(root->mlx, root->game->width * 40,
+		root->game->height * 40);
+	if (root->mlx_img == 0)
+		root_destroy(root, "mlx_new_image(): can't create an image", 0);
+}
+
+t_root	*root_init(char *filename)
+{
+	t_root			*root;
+
+	root = (t_root *)malloc(sizeof(t_root));
+	if (root == 0)
+		root_destroy(0, "root_init(): malloc()", errno);
+	root->game = 0;
+	root->mlx = 0;
+	root->mlx_win = 0;
+	root->mlx_img = 0;
+	root->player = 0;
+	root->exit = 0;
+	root->coll = 0;
+	root->wall = 0;
+	root->ground = 0;
+	game_init(root, filename);
+	renderer_init(root);
+	texture_init(root);
+	return (root);
+}
