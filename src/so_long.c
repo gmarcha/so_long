bc9dfb33ef25e12753578cdf952c2bafc5425f6c
gmarcha <gmarcha@github.com>
2021-06-13 02:20:43 +0200

    update so long: update player movement

--- src/so_long.c
+++ src/so_long.c
@@ -6,7 +6,7 @@
 /*   By: gamarcha <gamarcha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/07 18:36:29 by gamarcha          #+#    #+#             */
-/*   Updated: 2021/06/13 02:13:15 by gamarcha         ###   ########.fr       */
+/*   Updated: 2021/06/13 02:20:31 by gamarcha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -459,13 +459,13 @@ void	update(t_root *root)
 	if (root->game->player_up != 0)
 		if (root->game->map[y - 1][x] == 0)
 			root->game->player.y -= 1;
-	if (root->game->player_down != 0)
+	else if (root->game->player_down != 0)
 		if (root->game->map[y + 1][x] == 0)
 			root->game->player.y += 1;
-	if (root->game->player_left != 0)
+	else if (root->game->player_left != 0)
 		if (root->game->map[y][x - 1] == 0)
 			root->game->player.x -= 1;
-	if (root->game->player_right != 0)
+	else if (root->game->player_right != 0)
 		if (root->game->map[y][x + 1] == 0)
 			root->game->player.x += 1;
 	draw(root);
